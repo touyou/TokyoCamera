7086f748b69f6743fa1cd0a542c786e668e89d17
touyou <fujiyou1121@gmail.com>
2017-06-03 16:31:10 +0900

    add collection view

---
+++ TokyoCamera/TimelineCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  TimelineCollectionViewCell.swift
+//  TokyoCamera
+//
+//  Created by 藤井陽介 on 2017/06/03.
+//  Copyright © 2017年 touyou. All rights reserved.
+//
+
+import UIKit
+
+class TimelineCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+}
